cfc877261ea80ff33f9c2d65a641548de1e516ef
Honey Chauhan <hc924508@gmail.com>
2023-05-30 02:18:59 +0530

    sumbission

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -18,8 +18,8 @@ public class BlogController {
     @PostMapping
     public ResponseEntity createBlog(@RequestParam Integer userId , @RequestParam String title, @RequestParam String content) {
         // Create a blog and add it under given user
-        blogService.createAndReturnBlog(userId, title, content);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        Blog blog = blogService.createAndReturnBlog(userId, title, content);
+        return new ResponseEntity<>(blog,HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{blogId}")
